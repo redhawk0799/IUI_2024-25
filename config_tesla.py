61198fbc819fcb506ac0a79ea81c4e94431e0341
Flakon Selimi <selimi.flakon@gmail.com>
2025-01-23 20:31:23 +0100

    config for teslasuit added, data_transformer.py adapted to connect to teslasuit

---
+++ config_tesla.py
@@ -0,0 +1,15 @@
+# power for TeslaSuit
+power = {
+    "soft": 40,
+    "medium": 80,
+    "strong": 150
+}
+
+duration_of_touch = 500     # in ms, has to match with the interval in which the mean of the recording gets calculated!
+
+# threshold values for touch
+thresholds = {
+    "soft": [40, 100],
+    "medium": [100, 200],
+    "strong": [200]
+}
--- data_transformer.py
+++ data_transformer.py
@@ -6,10 +6,36 @@ import numpy as np
 from teslasuit_sdk import ts_api
 import teslasuit_sdk.subsystems.ts_haptic
 from teslasuit_sdk.ts_mapper import TsBone2dIndex
+import config_tesla
+
+connect = False
 
 # Define the IP and port to listen on
 LISTEN_IP = "127.0.0.1"  # Listen on all available interfaces
 LISTEN_PORT = 27152  # Replace with the port your server is sending data to
+
+# Teslasuit stuff
+areas = {
+    "LeftFrontThigh": 0,
+    "LeftBackThigh": 1,
+    "RightFrontThigh": 2,
+    "RightBackThigh": 3,
+    "LeftFrontLowerLeg": 4,
+    "LeftBackLowerLeg": 5,
+    "RightFrontLowerLeg": 6,
+    "RightBackLowerLeg": 7,
+    "Abdominal": 8,
+    "UpperChest": 9,
+    "Back": 11,
+    "LeftFrontUpperArm": 12,
+    "LeftBackUpperArm": 13,
+    "RightFrontUpperArm": 14,
+    "RightBackUpperArm": 15,
+    "LeftFrontLowerArm": 16,
+    "LeftBackLowerArm": 17,
+    "RightFrontLowerArm": 18,
+    "RightBackLowerArm": 19
+}
 
 
 def start_udp_listener():
@@ -48,8 +74,8 @@ def start_udp_listener():
         average_x = x_values.mean()
         average_y = y_values.mean()
 
-        average_x_data.append(average_x.tolist())
-        average_y_data.append(average_y.tolist())
+        average_x_data.append(round(average_x.tolist(), 3))
+        average_y_data.append(round(average_y.tolist(), 3))
 
     all_coordinates = list(zip(average_x_data, average_y_data)) # this groups the data as a list of coordinates
 
@@ -77,6 +103,31 @@ def start_udp_listener():
     finally:
         udp_socket.close()
 
+def connectToSuit():
+    location = ['Pectoral_L', 'Tricep_L', 'Shoulder_L', 'Bicep_L',
+                'BackUp_L', 'BackDown_L', 'Pectoral_R', 'Tricep_R', 'Shoulder_R',
+                'Bicep_R', 'BackUp_R', 'BackDown_R']
+
+    print("Initialize API")
+    api = ts_api.TsApi()
+
+    device = api.get_device_manager().get_or_wait_last_device_attached()
+    player = device.haptic
+    mapper = api.mapper
+
+    print("Setup channels to play and touch parameters")
+    layout = mapper.get_haptic_electric_channel_layout(device.get_mapping())
+    bones = mapper.get_layout_bones(layout)
+
+    signal = []
+
+    channels = mapper.get_bone_contents(bones[areas[signal[0]]])
+    params = player.create_touch_parameters(100, 40, signal[1])
+    playable_id = player.create_touch(params, channels, signal[2])
+    player.play_playable(playable_id)
 
 if __name__ == "__main__":
+    if connect:
+        connectToSuit()
+
     start_udp_listener()
