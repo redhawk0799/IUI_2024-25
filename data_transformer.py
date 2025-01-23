6f13f1f2d528c8fe3e228e04bda8a14a723712af
Flakon Selimi <selimi.flakon@gmail.com>
2025-01-23 20:13:15 +0100

    data transformation done, now we get all the coordinates(averages) in an interval of 0.25 seconds

--- data_transformer.py
+++ data_transformer.py
@@ -1,5 +1,7 @@
 import socket
 import struct
+import pandas as pd
+import numpy as np
 
 from teslasuit_sdk import ts_api
 import teslasuit_sdk.subsystems.ts_haptic
@@ -18,6 +20,41 @@ def start_udp_listener():
     udp_socket.bind((LISTEN_IP, LISTEN_PORT))
 
     print(f"Listening for UDP data on {LISTEN_IP}:{LISTEN_PORT}...")
+
+    csv_data = pd.read_csv('./dummy_data.csv', usecols=['currentTime', 'world_position_x', 'world_position_y'])
+
+    start_time = csv_data.iloc[0]['currentTime']
+    list_with_interval = []
+    split_data = []
+
+    for _, row in csv_data.iterrows():
+        if row['currentTime'] - start_time <= 0.25:
+            list_with_interval.append(row.tolist())
+        else:
+            split_data.append(list_with_interval)
+            list_with_interval = [row.tolist()]
+            start_time = row['currentTime']
+
+    if list_with_interval:
+        split_data.append(list_with_interval)
+
+    average_x_data = []
+    average_y_data = []
+
+    for data in split_data:
+        numpy_array = np.array(data)
+        x_values = numpy_array[:, 1]
+        y_values = numpy_array[:, 2]
+        average_x = x_values.mean()
+        average_y = y_values.mean()
+
+        average_x_data.append(average_x.tolist())
+        average_y_data.append(average_y.tolist())
+
+    all_coordinates = list(zip(average_x_data, average_y_data)) # this groups the data as a list of coordinates
+
+    print(f"coordinates: {all_coordinates}")
+    return
 
     try:
         while True:
